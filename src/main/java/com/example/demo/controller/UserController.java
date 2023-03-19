ef98dbf5fcc7bc8dc5af1983c7e730ff73ebfbe6
Ruwan Ranaweera <rwnranaweera@gmail.com>
2023-03-19 07:36:48 +0530

    create crud operation

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,49 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.UserDTO;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "api/v1/user")
+@CrossOrigin
+
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUser(){
+        return userService.getAllUsers();
+    }
+    @PostMapping("/saveUser")
+    public UserDTO saveUser(@RequestBody UserDTO userDTO){
+        return userService.saveUser(userDTO);
+    }
+
+    @PutMapping("/updateUser")
+    public UserDTO updateUser(@RequestBody UserDTO userDTO){
+        return userService.updateUser(userDTO);
+    }
+
+    @DeleteMapping("/deleteUser")
+    public boolean deleteUser(@RequestBody UserDTO userDTO){
+        return userService.deleteUser(userDTO);
+    }
+
+    @GetMapping("/getUserById/{userID}")
+    public UserDTO getUserByUserID(@PathVariable String userID){
+        return userService.getUserByUserId(userID);
+    }
+
+    // getUserByUserIdAndAddress
+
+    @GetMapping("/getUserByIdAndAddress/{userID}/{address}")
+    public UserDTO getUserByUserIdAndAddress(@PathVariable String userID,@PathVariable String address){
+        return userService.getUserByUserIdAndAddress(userID, address);
+    }
+}
+
